4bd706bb69f2d1591a2c671c48371a93bf074b22
Katiana Hanisch <katianahanisch@gmail.com>
2023-03-21 14:44:54 -0400

    modificacoes no layout

--- src/pages/QRCodePage/index.js
+++ src/pages/QRCodePage/index.js
@@ -11,6 +11,7 @@ import {
   ButtonTexto,
   ContainerQRCode,
   BackgroundQrCode,
+  TextoTitulo,
 } from "./styles";
 
 export default function QRCodePage() {
@@ -66,7 +67,7 @@ export default function QRCodePage() {
   return (
     <Container>
       <ContainerQRCodeHead>
-        <Texto>QR-Code</Texto>
+        <TextoTitulo>QR-Code</TextoTitulo>
       </ContainerQRCodeHead>
       <ContainerQRCode>
         <BackgroundQrCode>
