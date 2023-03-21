e437af50814b7b9a631bf2177406706f40ee3cab
Katiana Hanisch <katianahanisch@gmail.com>
2023-03-21 08:11:50 -0400

    adicionando mensagem de erro ao logar

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,19 @@
 import React, { useState, useContext } from "react";
 import { Platform, ActivityIndicator } from "react-native";
 
+import {
+  Stack,
+  Alert,
+  IconButton,
+  HStack,
+  VStack,
+  CloseIcon,
+  Text,
+  Center,
+  NativeBaseProvider,
+  Box,
+} from "native-base";
+
 import { AuthContext } from "../../context/auth";
 
 import {
@@ -18,12 +31,15 @@ export default function SignIn() {
 
   const [ra, setRa] = useState("");
   const [password, setPassword] = useState("");
+  const [show, setShow] = React.useState(true);
 
   function handleLogin() {
     if (ra === "" || password === "") return;
 
     signIn(ra, password);
   }
+
+  console.log(erroLogin);
 
   return (
     <Background>
@@ -46,6 +62,32 @@ export default function SignIn() {
             keyboardType="numeric"
           />
         </AreaInput>
+        <NativeBaseProvider>
+          <Box w="100%" alignItems="center" mx="auto">
+            <Alert w="100%" status="error">
+              <VStack space={2} flexShrink={2} w="100%">
+                <HStack flexShrink={1} space={2} justifyContent="space-between">
+                  <HStack space={2} flexShrink={1}>
+                    <Alert.Icon mt="1" />
+                    <Text fontSize="md" color="coolGray.800">
+                      Usuário ou senha incorreta
+                    </Text>
+                  </HStack>
+                  <IconButton
+                    variant="unstyled"
+                    _focus={{
+                      borderWidth: 0,
+                    }}
+                    icon={<CloseIcon size="3" />}
+                    _icon={{
+                      color: "coolGray.600",
+                    }}
+                  />
+                </HStack>
+              </VStack>
+            </Alert>
+          </Box>
+        </NativeBaseProvider>
         <SubmitButton activeOpacity={0.7} onPress={handleLogin}>
           {loadingAuth ? (
             <ActivityIndicator size={20} color="#FFF" />
