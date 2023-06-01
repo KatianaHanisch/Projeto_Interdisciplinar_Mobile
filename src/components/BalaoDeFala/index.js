77c9b069df2ed0fac2e6435df8ede8a84300fbab
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-06-01 09:13:44 -0400

    Alterado style do balão de fala

--- src/components/BalaoDeFala/index.js
+++ src/components/BalaoDeFala/index.js
@@ -1,91 +1,94 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image, Dimensions } from "react-native";
 
-export default function SpeechBubble({ message }) {
+const { width } = Dimensions.get('window');
+
+export default function BalaoDeFala({ message }) {
   const [displayText, setDisplayText] = useState("");
   const [textComplete, setTextComplete] = useState(false);
 
   const animateText = () => {
-    const text = message || ''
-    var i = 0
+    const text = message || '';
+    let i = 0;
 
-    setTextComplete(false)
-    setDisplayText('')
+    setTextComplete(false);
+    setDisplayText('');
 
     const interval = setInterval(() => {
       setDisplayText((prevText) => prevText + text[i]);
       i++;
       if (i === text.length) {
-        clearInterval(interval)
-        setTextComplete(true)
+        clearInterval(interval);
+        setTextComplete(true);
       }
-    }, 70)
-  }
+    }, 70);
+  };
 
   useEffect(() => {
-    animateText()
-  }, [])
+    animateText();
+  }, []);
 
   const handleImagePress = () => {
     if (textComplete) {
-      animateText()
+      animateText();
     }
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "flex-end",
-        flexDirection: 'row',
-        marginBottom: 35,
-        marginRight: -80,
-      }}
-    >
-      <View style={{
-        marginBottom: 50
-      }}>
-        <View style={styles.balaoDeFala}>
-          <Text>
-            {displayText}
-          </Text>
+    <View style={styles.container}>
+      <View style={styles.balaoDeFala}>
+        <Text>{displayText}</Text>
+        <View style={styles.conteinerBalao}>
+          <View style={styles.seta} />
         </View>
-        <View style={styles.seta} />
       </View>
-      <TouchableOpacity onPress={() => handleImagePress()}>
+      <TouchableOpacity onPress={() => handleImagePress()} style={styles.imageContainer}>
         <Image
           source={require("../../../assets/dog.webp")}
-          style={{
-            width: 80,
-            height: 80,
-          }}
+          style={styles.image}
+          resizeMode="cover"
         />
       </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-end",
+    flexDirection: "row",
+    marginBottom: width * 0.1,
+    marginRight: -width * 0.2,
+  },
+  conteinerBalao: {
+  },
   balaoDeFala: {
-    position: 'relative',
-    width: 220,
+    width: width * 0.6,
     padding: 10,
-    backgroundColor: '#f2f2f2',
+    backgroundColor: "#f2f2f2",
     borderRadius: 10,
+    marginBottom: 30
   },
   seta: {
-    position: 'absolute',
-    top: '50%',
-    right: -15,
-    marginTop: -30,
-    transform: 'rotate(122deg)',
+    position: "absolute",
+    left: '99%',
+    marginTop: -20,
+    transform: [{ rotate: "122deg" }],
     borderLeftWidth: 15,
     borderRightWidth: 15,
     borderBottomWidth: 30,
-    borderStyle: 'solid',
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: '#f2f2f2',
+    borderStyle: "solid",
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: "#f2f2f2",
   },
-})
+  imageContainer: {
+    marginLeft: width * 0.01,
+  },
+  image: {
+    width: width * 0.2,
+    height: width * 0.2,
+  },
+});
